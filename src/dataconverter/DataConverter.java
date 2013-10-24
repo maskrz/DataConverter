a5eedd878ef597513f0484975feaf57d9edbbc9e
Maciej Skrzypinski <maskrz@gmail.com>
2013-10-24 18:58:17 +0200

    initial commit

---
+++ src/dataconverter/DataConverter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataconverter;
+
+/**
+ *
+ * @author Skrzypek
+ */
+public class DataConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
